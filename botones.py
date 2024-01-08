6064ccc8b14fc4d6333292d31fbd19753c9d4549
snLionel90 <leonjuman@gmail.com>
2024-01-08 20:40:48 +0100

    CHIPI CHIPI CHAPA CHAPA DUBI DUBI DABA DABA MAGICO MI DUBI DUBI BOOM BOOM BOOM BOOM

--- botones.py
+++ botones.py
@@ -2,6 +2,22 @@ import os
 import flet as ft
 
 def main(page : ft.page):
+
+    def btn_apagar(e):
+        mensaje.value = "Apagando"
+        page.update()
+        os.system('Shutdown /s t -1')
+
+    def btn_reinicio(e):
+        mensaje.value = "reiniciando..."
+        page.update()
+        os.system('Shutdown /r t -1')
+
+    def btn_f(e):
+        mensaje.value = "Te Jodes, ella no te ama por GILIPOLLAS"
+        page.update()
+        os.system('')
+
     btn_apagar = ft.ElevatedButton("Apagar", icon=ft.icons.ADD_TO_HOME_SCREEN_ROUNDED)
     btn_reinicio = ft.ElevatedButton("Reincia", icon=ft.icons.ADD_TO_HOME_SCREEN_ROUNDED)
     btn_f = ft.ElevatedButton("F", icon=ft.icons.ADD_TO_HOME_SCREEN_ROUNDED)
@@ -9,7 +25,9 @@ def main(page : ft.page):
     mensaje = ft.Text()
 
     page.add(ft.Row(controls=[
-        btn_apagar,btn_f,btn_reinicio
+        btn_apagar,
+        btn_f,
+        btn_reinicio
         ]),
         mensaje)
     
